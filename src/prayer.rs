4a5802484186b4331eebb8087d6b53c96f6db291
Muhammad Ragib Hasin <ragib.hasin@gmail.com>
2020-09-29 17:03:07 +0600

    chore: use RagibHasin/salah-rs@uptosnuff

--- src/prayer.rs
+++ src/prayer.rs
@@ -1,65 +1,41 @@
 use indexmap::IndexMap;
-use salah::prelude::{Configuration, Coordinates, Prayer, PrayerSchedule, PrayerTimes};
+use salah::{Coordinates, DateTime, Local, Parameters, Prayer, PrayerTimes};
 
 use crate::config;
-use crate::util::to_local;
 
 /// Setup a prayer config, and get all its time.
-fn get_prayers_time(config: config::Config) -> Result<salah::PrayerTimes, String> {
+fn get_prayers_time(config: config::Config) -> PrayerTimes {
     let city = Coordinates::new(config.latitude, config.longitude);
-    let date = salah::Utc::today();
-    let params = Configuration::with(config.method, config.madhab);
-    let prayers = PrayerSchedule::new()
-        .on(date)
-        .for_location(city)
-        .with_configuration(params)
-        .calculate();
-    return prayers;
+    let date = Local::today();
+    let params = Parameters::with(config.method, config.madhab);
+
+    PrayerTimes::calculate(date, city, params)
 }
 
 /// Returns all prayers.
 pub fn get_all_prayers() -> IndexMap<String, salah::DateTime<salah::Local>> {
     let mut prayers = IndexMap::new();
-    let prayers_time = get_prayers_time(config::get_config());
+    let prayer = get_prayers_time(config::get_config());
 
-    match prayers_time {
-        Ok(prayer) => {
-            prayers.insert(Prayer::Fajr.name(), to_local(prayer.time(Prayer::Fajr)));
-            prayers.insert(
-                Prayer::Sunrise.name(),
-                to_local(prayer.time(Prayer::Sunrise)),
-            );
-            prayers.insert(Prayer::Dhuhr.name(), to_local(prayer.time(Prayer::Dhuhr)));
-            prayers.insert(Prayer::Asr.name(), to_local(prayer.time(Prayer::Asr)));
-            prayers.insert(
-                Prayer::Maghrib.name(),
-                to_local(prayer.time(Prayer::Maghrib)),
-            );
-            prayers.insert(Prayer::Isha.name(), to_local(prayer.time(Prayer::Isha)));
-            prayers.insert(Prayer::Qiyam.name(), to_local(prayer.time(Prayer::Qiyam)));
-        }
-        Err(error) => println!("Could not calculate prayer times: {}", error),
-    }
+    prayers.insert(Prayer::Fajr.name(), prayer.time(Prayer::Fajr));
+    prayers.insert(Prayer::Sunrise.name(), prayer.time(Prayer::Sunrise));
+    prayers.insert(Prayer::Dhuhr.name(), prayer.time(Prayer::Dhuhr));
+    prayers.insert(Prayer::Asr.name(), prayer.time(Prayer::Asr));
+    prayers.insert(Prayer::Maghrib.name(), prayer.time(Prayer::Maghrib));
+    prayers.insert(Prayer::Isha.name(), prayer.time(Prayer::Isha));
+    prayers.insert(Prayer::Qiyam.name(), prayer.time(Prayer::Qiyam));
 
-    return prayers;
+    prayers
 }
 
 /// Returns current prayer.
-pub fn get_current_prayer() -> Result<PrayerTimes, String> {
-    let prayers_time = get_prayers_time(config::get_config());
-
-    match prayers_time {
-        Ok(prayer) => Ok(prayer),
-        Err(error) => Err(format!("Could not calculate prayer times: {}", error)),
-    }
+pub fn get_current_prayer() -> PrayerTimes {
+    get_prayers_time(config::get_config())
 }
 
 /// Returns current prayer.
-pub fn get_next_prayer() -> Result<(String, salah::DateTime<salah::Local>), String> {
-    let prayers_time = get_prayers_time(config::get_config());
+pub fn get_next_prayer() -> (String, DateTime<Local>) {
+    let prayer = get_prayers_time(config::get_config());
 
-    match prayers_time {
-        Ok(prayer) => Ok((prayer.next().name(), to_local(prayer.time(prayer.next())))),
-        Err(error) => Err(format!("Could not calculate prayer times: {}", error)),
-    }
+    (prayer.next().name(), prayer.time(prayer.next()))
 }
