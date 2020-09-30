c4d38a8286b953ae5bf43fda19a03eac333eb3c7
azzamsa <me@azzamsa.com>
2020-09-30 07:14:27 +0700

    build: add warning to update related release file

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,8 @@
 VERSION=`awk '/version = "[0-9]\.[0-9]\.[0-9]"/' Cargo.toml | awk -F\" '{print $2}'`
 
+# init
+cargo fmt
+
 # GNU/Linux
 cargo build --release
 strip target/release/bilal
@@ -8,3 +11,7 @@ mv target/release/bilal target/release/bilal-$VERSION
 # Windows PC
 cargo build --release --target=x86_64-pc-windows-gnu
 mv target/x86_64-pc-windows-gnu/release/bilal.exe target/x86_64-pc-windows-gnu/release/bilal-$VERSION.exe
+
+# don't forget!
+echo "Check the changelog!" >> CHANGELOG.md
+echo "Check the version!" >> Cargo.toml
