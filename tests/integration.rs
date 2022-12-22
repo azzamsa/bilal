d0ffc2b9f986bf8849049138d5fe51c3e990fda0
azzamsa <me@azzamsa.com>
2022-12-22 18:37:48 +0700

    test: don't hardcode crate name

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,10 +1,11 @@
-use assert_cmd::prelude::*;
-use predicates::prelude::*;
 use std::{env, error::Error, process::Command};
+
+use assert_cmd::{crate_name, prelude::*};
+use predicates::prelude::*;
 
 #[test]
 fn help() -> Result<(), Box<dyn Error>> {
-    let mut cmd = Command::cargo_bin("bilal")?;
+    let mut cmd = Command::cargo_bin(crate_name!())?;
     cmd.arg("-h");
     cmd.assert()
         .success()
@@ -17,7 +18,7 @@ fn all() -> Result<(), Box<dyn Error>> {
     env::set_var("HOME", "./tests");
     env::set_var("APPDATA", "./tests");
 
-    let mut cmd = Command::cargo_bin("bilal")?;
+    let mut cmd = Command::cargo_bin(crate_name!())?;
     cmd.arg("all");
     cmd.assert()
         .success()
@@ -30,7 +31,7 @@ fn current() -> Result<(), Box<dyn Error>> {
     env::set_var("HOME", "./tests");
     env::set_var("APPDATA", "./tests");
 
-    let mut cmd = Command::cargo_bin("bilal")?;
+    let mut cmd = Command::cargo_bin(crate_name!())?;
     cmd.arg("current").arg("--json");
     // \u{23fa} : ⏺
     cmd.assert()
@@ -44,7 +45,7 @@ fn next() -> Result<(), Box<dyn Error>> {
     env::set_var("HOME", "./tests");
     env::set_var("APPDATA", "./tests");
 
-    let mut cmd = Command::cargo_bin("bilal")?;
+    let mut cmd = Command::cargo_bin(crate_name!())?;
     cmd.arg("next").arg("--json");
     // \u{25b6} : ▶
     cmd.assert()
